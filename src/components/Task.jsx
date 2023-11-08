39908e598f14c036db28c4afb1f20081903dd2eb
Vitor Coutinho Fernandes <c.svitor@yahoo.com.br>
2023-11-08 00:08:40 -0300

    refactor: rename handle function name

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -9,13 +9,13 @@ import { useStore } from '../store'
 export default function Task({ title, state }) {
   const deleteTask = useStore((store) => store.deleteTask)
 
-  const handleOnClick = () => deleteTask(title, state) 
+  const handleOnDeleteIconClick = () => deleteTask(title, state) 
 
   return (
     <div className="bg-slate-900 rounded text-white flex flex-col justify-between max-h-20 p-2 gap-2">
       <header className="flex justify-between">
         <h4>{ title }</h4>
-        <button onClick={handleOnClick}><TrashIcon className="w-6 h-6 text-slate-950" /></button>
+        <button onClick={handleOnDeleteIconClick}><TrashIcon className="w-6 h-6 text-slate-950" /></button>
       </header>
       <div className="flex justify-end">
         <span className={classNames("status", state)}>{ state }</span>
