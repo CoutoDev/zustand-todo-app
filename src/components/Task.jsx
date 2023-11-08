3f77645b07531f791445807f3a2f9dfe7f07cc2e
Vitor Coutinho Fernandes <c.svitor@yahoo.com.br>
2023-11-08 00:05:45 -0300

    feat: add delete button and fix duplicated status

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,20 +1,24 @@
 import classNames from 'classnames'
+import { TrashIcon } from '@heroicons/react/24/solid'
+
 import './Task.css'
 import { useStore } from '../store'
-import { useShallow } from 'zustand/react/shallow'
 
 
-export default function Task({ title }) {
-  const task = useStore(
-    useShallow((store) => store.tasks.find(task => task.title === title))
-  );
+
+export default function Task({ title, state }) {
+  const deleteTask = useStore((store) => store.deleteTask)
+
+  const handleOnClick = () => deleteTask(title, state) 
 
   return (
-    <div className="bg-slate-900 rounded text-white flex flex-col justify-between max-h-20 p-2">
-      <div>{ task.title }</div>
-      <div className="flex justify-between">
-        <div></div>
-        <div className={classNames("status", task.state)}>{ task.state }</div>
+    <div className="bg-slate-900 rounded text-white flex flex-col justify-between max-h-20 p-2 gap-2">
+      <header className="flex justify-between">
+        <h4>{ title }</h4>
+        <button onClick={handleOnClick}><TrashIcon className="w-6 h-6 text-slate-950" /></button>
+      </header>
+      <div className="flex justify-end">
+        <span className={classNames("status", state)}>{ state }</span>
       </div>
     </div>
   )
